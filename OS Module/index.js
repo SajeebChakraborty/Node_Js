a5129aec1fd1e099f847fd0b7453ed762a7c0318
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2022-09-07 00:30:31 +0600

    OS Module get operating system information

---
+++ OS Module/index.js
@@ -0,0 +1,21 @@
+/*
+
+    Os module needed for information about operating system
+
+
+*/
+
+
+const os = require('os');
+
+//console.log(os);
+
+console.log(os.userInfo());
+
+console.log(os.homedir());
+
+console.log(os.hostname());
+
+console.log(os.totalmem());
+
+console.log(os.freemem());
